40f6856b891c8818e4646a634ed643d48f44a245
Dalfior <aecio.dalfior@gmail.com>
2021-07-23 15:57:42 -0300

    Montando o layout do projeto, parte 3. 15m40s.

--- app/src/main/java/br/com/dio/businesscard/ui/MainActivity.kt
+++ app/src/main/java/br/com/dio/businesscard/ui/MainActivity.kt
@@ -3,12 +3,17 @@ package br.com.dio.businesscard.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import br.com.dio.businesscard.R
+import androidx.activity.viewModels
+import br.com.dio.businesscard.App
 import br.com.dio.businesscard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private val binding by lazy {ActivityMainBinding.inflate(layoutInflater)}
+    private val mainViewModel: MainViewModel by viewModels {
+        MainViewModelFactory((application as App).repository)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -19,5 +24,10 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this@MainActivity, AddBusinessCardActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun getAssBusinessCard() {
+        mainViewModel.getAll().observe(this, {
+        })
     }
 }
